100181a488eabb96cbdaaac07212e76f2b883bcd
Juan Barezzi <juanbarezzi.dev@gmail.com>
2023-05-20 12:30:46 -0300

    feat: create empty timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="flex flex-col items-start justify-between px-28 py-16"></div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[350px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
